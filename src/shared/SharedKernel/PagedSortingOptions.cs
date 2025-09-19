4b8f82d0fa2a14ffc23aac28eae9c6dad5d32c6c
Andrii Shandryk <shandrykjj512@gmail.com>
2025-09-19 22:28:43 +0300

    feat(core): add SharedKernel proj with error handling and pagination features

---
+++ src/shared/SharedKernel/PagedSortingOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace SharedKernel;
+public class PagedSortingOptions
+{
+    public string? SortField { get; set; }
+    public SortOrder? SortOrder { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+}
+
+public enum SortOrder
+{
+    Unsorted,
+    Ascending,
+    Descending
+}
