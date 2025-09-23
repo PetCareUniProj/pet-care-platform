bba00acce8bb5647e2dd50808ea064d6707a0047
Andrii Shandryk <shandrykjj512@gmail.com>
2025-09-23 21:08:04 +0300

    feat(auth): enhance authentication and add users
    * Added users with roles in `realm-export.json`.
    * Updated `AuthenticationExtensions.cs` with new config values and improved JWT authentication.
    * Enhanced role handling from `resource_access` claim.

--- src/aspire/ServiceDefaults/AuthenticationExtensions.cs
+++ src/aspire/ServiceDefaults/AuthenticationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,67 +8,67 @@ using Microsoft.Extensions.Hosting;
 namespace ServiceDefaults;
 public static class AuthenticationExtensions
 {
+
     public static IServiceCollection AddDefaultAuthentication(this IHostApplicationBuilder builder)
     {
         var services = builder.Services;
-        var configuration = builder.Configuration;
+        IConfiguration configuration = builder.Configuration;
 
         var identitySection = configuration.GetSection("Identity");
-        var clientId = identitySection.GetRequiredValue("ClientId");
         if (!identitySection.Exists())
         {
             return services;
         }
 
+        var clientId = identitySection.GetRequiredValue("ClientId");
+        var url = identitySection.GetRequiredValue("Url");
+        var realm = identitySection.GetRequiredValue("Realm");
+        var authority = $"{url}/realms/{realm}";
+        var audience = identitySection.GetValue<string>("Audience") ?? clientId;
+
         services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddKeycloakJwtBearer(
-                serviceName: identitySection.GetRequiredValue("ServiceName"),
-                realm: identitySection.GetRequiredValue("Realm"),
-                options =>
+                options.Authority = authority;
+                options.Audience = audience;
+                options.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
+
+                options.TokenValidationParameters.ValidAudiences = new[] { "account", clientId };
+
+                options.Events = new JwtBearerEvents
                 {
-
-                    if (builder.Environment.IsDevelopment())
+                    OnTokenValidated = context =>
                     {
-                        options.TokenValidationParameters.ValidAudiences = new[] { "account", clientId };
-                        options.RequireHttpsMetadata = false;
-                    }
-                    else
-                    {
-                        options.Audience = identitySection.GetRequiredValue("ClientId");
+                        var claimsIdentity = context.Principal!.Identity as ClaimsIdentity;
 
-                    }
-
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnTokenValidated = context =>
+                        // Add roles from resource_access.{clientId}
+                        var resourceAccess = context.Principal.FindFirst("resource_access")?.Value;
+                        if (resourceAccess is not null)
                         {
-                            var claimsIdentity = context.Principal!.Identity as ClaimsIdentity;
-                            var resourceAccess = context.Principal.FindFirst("resource_access")?.Value;
-                            if (resourceAccess != null)
+                            using var doc = JsonDocument.Parse(resourceAccess);
+                            if (doc.RootElement.TryGetProperty(clientId, out var clientResource))
                             {
-                                var obj = System.Text.Json.JsonDocument.Parse(resourceAccess);
-                                if (obj.RootElement.TryGetProperty(clientId, out var catalogApi))
+                                if (clientResource.TryGetProperty("roles", out var clientRoles))
                                 {
-                                    if (catalogApi.TryGetProperty("roles", out var roles))
+                                    foreach (var role in clientRoles.EnumerateArray())
                                     {
-                                        foreach (var role in roles.EnumerateArray())
-                                        {
-                                            claimsIdentity!.AddClaim(new Claim(ClaimsIdentity.DefaultRoleClaimType, role.GetString()!));
-                                        }
+                                        claimsIdentity!.AddClaim(new Claim(ClaimsIdentity.DefaultRoleClaimType, role.GetString()!));
                                     }
                                 }
                             }
-
-                            return Task.CompletedTask;
                         }
-                    };
-                });
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
         return services;
     }
 }
+
