cf4373c4b97f67833f80c647a88aed20e63f2e2e
Andrii Shandryk(Invop) <shandrykjj512@gmail.com>
2025-10-14 12:41:44 +0300

    feat(api): Integrate Scalar API and update dependencies
    Integrate Scalar API with debugging, HTTPS preference, and OAuth2
    authorization code flow. Add API references for `catalogApi` and
    `orderingApi` with OpenAPI route patterns.

--- src/aspire/AppHost/AppHost.cs
+++ src/aspire/AppHost/AppHost.cs
@@ -1,4 +1,5 @@
-using Projects;
+﻿using Projects;
+using Scalar.Aspire;
 
 var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddRedis("redis");
@@ -40,8 +41,8 @@ builder.AddNpmApp("subscription-api", "../../services/subscription", "start:dev"
     .WaitFor(keycloak).WithEnvironment("Identity__Url", identityEndpoint);
 
 var catalogApi = builder.AddProject<Catalog_Api>("catalog-api")
-    .WithReference(catalogDb)
     .WithReference(rabbitMq).WaitFor(rabbitMq)
+    .WithReference(catalogDb).WaitFor(catalogDb)
     .WaitFor(keycloak).WithEnvironment("Identity__Url", identityEndpoint);
 
 builder.AddNpmApp("store-web", "../../store", "start:dev")
@@ -51,5 +52,38 @@ builder.AddNpmApp("store-web", "../../store", "start:dev")
     .WithReference(basketApi).WaitFor(basketApi)
     .WithReference(catalogApi).WaitFor(catalogApi)
     .WaitFor(keycloak).WithEnvironment("Identity__Url", identityEndpoint);
+
+// Add Scalar API Reference with debugging enabled
+var scalar = builder.AddScalarApiReference(options =>
+{
+    options.WithTheme(ScalarTheme.Saturn);
+
+    options
+        .PreferHttpsEndpoint() // Use HTTPS endpoints when available
+        .AllowSelfSignedCertificates(); // Trust self-signed certificates
+
+    options.AddPreferredSecuritySchemes("oauth2")
+            .AddAuthorizationCodeFlow("oauth2", flow =>
+            {
+                flow.WithClientId("public-client-web");
+                flow.WithSelectedScopes("openid", "profile");
+            });
+
+})
+.WithReference(keycloak)
+.WithExternalHttpEndpoints();
+
+scalar.WithApiReference(catalogApi, options =>
+{
+    options
+        .AddDocument("v1", "Catalog API v1")
+        .WithOpenApiRoutePattern("/openapi/{documentName}.json");
+});
+scalar.WithApiReference(orderingApi, options =>
+{
+    options
+        .AddDocument("v1", "Ordering API v1")
+        .WithOpenApiRoutePattern("/openapi/{documentName}.json");
+});
 
 builder.Build().Run();
