5dbc58bc065a0e7de6f2b55e1e571a1c2fc42043
Andrii Shandryk(Invop) <shandrykjj512@gmail.com>
2025-11-27 14:35:22 +0200

    some filters

--- src/services/ordering/Ordering.Api/Endpoints/Orders/GetOrdersByUser.cs
+++ src/services/ordering/Ordering.Api/Endpoints/Orders/GetOrdersByUser.cs
@@ -6,28 +6,51 @@ using Ordering.Api.Infrastructure;
 using Ordering.Application.Abstractions.Authentication;
 using Ordering.Application.Orders;
 using Ordering.Application.Orders.GetByUser;
+using Ordering.Domain.Orders;
 
 namespace Ordering.Api.Endpoints.Orders;
 
 internal sealed class GetOrdersByUser : IEndpoint
 {
     public const string Name = "GetOrdersByUser";
+
+    public sealed record GetOrdersByUserRequest : PagedRequest
+    {
+        public string? SortBy { get; init; }
+        public OrderStatus[]? Statuses { get; init; }
+        public bool? IsRecurring { get; init; }
+    }
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapGet(ApiEndpoints.Orders.GetByUser,
-            async (IIdentityService identityService, IMediator mediator, CancellationToken cancellationToken) =>
-        {
-            var userId = identityService.GetUserIdentity();
-            if (userId == Guid.Empty)
+            async (
+                [AsParameters] GetOrdersByUserRequest request,
+                IIdentityService identityService,
+                IMediator mediator,
+                CancellationToken cancellationToken) =>
             {
-                return Results.Unauthorized();
-            }
+                var userId = identityService.GetUserIdentity();
+                if (userId == Guid.Empty)
+                {
+                    return Results.Unauthorized();
+                }
 
-            var orders = await mediator.Send(new GetOrdersByUserQuery() { UserId = userId }, cancellationToken);
-            return orders.Match(
-            orders => Results.Ok(orders),
-            CustomResults.Problem);
-        })
+                var query = new GetOrdersByUserQuery
+                {
+                    UserId = userId,
+                    SortField = request.SortBy?.Trim('+', '-'),
+                    SortOrder = request.SortBy is null ? SortOrder.Unsorted :
+            request.SortBy.StartsWith('-') ? SortOrder.Descending : SortOrder.Ascending,
+                    Page = request.Page.GetValueOrDefault(PagedRequest.DefaultPage),
+                    PageSize = request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize),
+                    Statuses = request.Statuses,
+                    IsRecurring = request.IsRecurring
+                };
+                var orders = await mediator.Send(query, cancellationToken);
+                return orders.Match(
+                orders => Results.Ok(orders),
+                CustomResults.Problem);
+            })
         .WithName(Name)
         .WithTags(Tags.Orders)
         .WithSummary("Get Orders for the Authenticated User")
